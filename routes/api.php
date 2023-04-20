5ec3e9ea780549df983e3d503e369b5acd002f42
Rami Mustaklem <ramimustaklem@gmail.com>
2023-04-20 12:21:26 +0300

    Install laravel breeze API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // /api/admin/...
-Route::prefix('admin')->middleware(['auth:sanctum'])->group(function () {
+Route::prefix('admin')->middleware(['auth'])->group(function () {
     Route::apiResources([
         'products' => ProductController::class,
         // categories
